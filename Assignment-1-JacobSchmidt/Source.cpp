3bf9f68b8ad9549300cd8af72ebae8418220d00d
Jacob Schmidt <jacob.schmidt@lethbridgecollege.ca>
2021-09-26 15:32:14 -0600

    Add project files.

---
+++ Assignment-1-JacobSchmidt/Source.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double Inum;
+    int Rnum, m, d, c, l, x, v, i, n;
+
+    cout << "Enter a number: ";
+    cin >> Rnum;
+    Rnum = (int)Rnum;
+
+    m = Rnum / 1000;
+    d = ((Rnum % 1000) / 500);
+    c = ((Rnum % 500) / 100);
+    l = ((Rnum % 100) / 50);
+    x = ((Rnum % 50) / 10);
+    v = ((Rnum % 10) / 5);
+    i = (Rnum % 5);
+    n = m + d + c + l + x + v + i;
+
+    while (n > 0) {
+        if (Rnum >= 4000) {
+            cout << "error";
+        }
+
+        else if (m > 0) {
+            cout << "M";
+            m--;
+            n--;
+        }
+        else if (d > 0) {
+            cout << "D";
+            d--;
+            n--;
+        }
+        else if (c > 0) {
+            cout << "C";
+            c--;
+            n--;
+        }
+        else if (l > 0) {
+            cout << "L";
+            l--;
+            n--;
+        }
+        else if (x > 0) {
+            cout << "X";
+            x--;
+            n--;
+        }
+        else if (v > 0) {
+            cout << "V";
+            v--;
+            n--;
+        }
+        else if (i > 0) {
+            cout << "I";
+            i--;
+            n--;
+        }
+        break;
+    }
+    return 0;
+}
